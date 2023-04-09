c6a0d63a824e0263d0bb41d7a8b86d83419ca9aa
rhu3 <rhu3@uw.edu>
2023-04-08 21:19:30 -0700

    Fix errors

--- question-10.sql
+++ question-10.sql
@@ -1,2 +1,2 @@
 INSERT INTO persons (first_name, last_name, title, age, gender, city, address, email)
-VALUES ('YourFirstName', 'YourLastName', 'YourTitle', YourAge, 'YourGender', 'YourCity', 'YourAddress', 'YourEmail');
+VALUES ('Gloria', 'Hu', 'Student', 23, 'F', 'Seattle', '123 Main St', 'rhu3@uw.edu');
